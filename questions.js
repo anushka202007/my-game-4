2dffdb971f6b98c40504d935df62a70ca3ef7c83
anushka202007 <71169908+anushka202007@users.noreply.github.com>
2021-06-02 19:34:19 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,251 @@
+var Questions = [
+  {
+    title: "Who was the 1st Chief Minister of Karnataka?",
+    choices: ["K. Chengalaraya Reddy", "K. Hanumanthaiah", "Kadidal Manjappa ", "R. Gundu Rao"],
+    answer: "K. Chengalaraya Reddy"
+  },
+  {
+    title: "When did Karnataka form a partial state of India ?",
+    choices: ["1 November, 1956", "12 November 1965", "15 October 1956", "15 October 1956"],
+    answer: "1 November, 1956"
+  },
+  {
+    title: "Who is known as the founder of Vijayanagara Empire?",
+    choices: ["Bukka Raya I ", "Harihara I", "Ramachandra Raya", "Deva Raya I "],
+    answer: "Harihara I"
+  },
+  {
+    title: "The Gol Gumbaz is located in the city? ",
+    choices: ["Davangere", "Raichur", "Gulbarga", "Bijapur"],
+    answer: "Bijapur"
+  },
+  {
+    title: "Nandi Hills, which is a famous tourist spot is located in which district of Karnataka?",
+    choices: ["Kolar", "Ramanagar","Chickaballapur","Tumkur"],
+    answer: "Chickaballapur"
+  },
+  {
+    title: "At which among the following places, there is no bench of Bombay High court?",
+    choices: ["Nasik", "Nagpur","Panji","Aurangabad"],
+    answer: "Nasik"
+  },
+  {
+    title: "Which of the following Dance form is Popular in Maharashtra state?",
+    choices: ["Lavani", "Kuchipudi","Mohiniattam","None of these"],
+    answer: "Lavani"
+  },
+  {
+    title: "Which city is famous as Orange city?",
+    choices: ["Nagpur", "Pune","Solapur","Nashik"],
+    answer: "Nagpur"
+  },
+  {
+    title: "The most important river of Maharashtra which is a lifeline of the whole South Maharashtra is? ",
+    choices: ["Koyna", "Krishna","Warna","Wardha"],
+    answer: "Krishna"
+  },
+  {
+    title: "Who was the first Chief Minister of Maharashtra ",
+    choices: ["Yashwantrao Chavan", "P. K. Sawant","Vasantrao Naik","Shankarrao Chavan"],
+    answer: "Yashwantrao Chavan"
+  },
+  {
+    title: "Who is the first woman Chief Minister of Delhi ",
+    choices: ["Sushma Swaraj", "Sheila Dikshit","Meira Kumar","Pratibha Patil"],
+    answer: "Sushma Swaraj"
+  },
+  {
+    title: "Is there a state located in the north, west, and south of Delhi?",
+    choices: ["Himachal Pradesh", "Uttar Pradesh","Haryana","Uttarakhand"],
+    answer: "Haryana"
+  },
+  {
+    title: "What was the old name of Delhi? ",
+    choices: ["Patliputra", "Magadh","Indraprastha","Hastinapur"],
+    answer: "Indraprastha"
+  },
+  {
+    title: "Who was the founder of Khilji Dynasty? ",
+    choices: ["Malik Firoz", "Jalal-ud-din Firoz Shah","Only A","Both A & B"],
+    answer: "Both A & B"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Which is the smallest district in Tamil Nadu by area?",
+    choices: ["Chennai", "Villupuram","Erode","Salem"],
+    answer: "Chennai"
+  },
+  {
+    title: "Tamil Nadu has how many International Airports? ",
+    choices: ["2", "1","4","3"],
+    answer: "4"
+  },
+  {
+    title: "Which water body is to the east of Tamil Nadu? ",
+    choices: ["Gulf of Kutch", "Bay of Bengal","Gulf of Cambay","Arabian Sea"],
+    answer: "Bay of Bengal"
+  },
+  {
+    title: "Which one is recognised as state fruit in Tamil Nadu? ",
+    choices: ["Orange", "Banana","Jackfruit","Mango"],
+    answer: "Jackfruit"
+  },
+  {
+    title: "In which year, name Chennai from Madras was officially changed?",
+    choices: ["1998", "1995","2005","2014"],
+    answer: "1995"
+  },
+  {
+    title: "Kerala is ________ largest state by population.",
+    choices: ["Tenth", "Eleventh","Thirteenth","Seventeenth"],
+    answer: "Thirteenth"
+  },
+  {
+    title: "Which festival of Kerala marks the new year? ",
+    choices: ["Onam", "Vishu","Arattu","Pooram"],
+    answer: "Vishu"
+  },
+ 
+  {
+    title: "Which one is the longest river in Kerala? ",
+    choices: ["Cochin", "Vizhinjam","Beypore","Azhikkal"],
+    answer: "Cochin"
+  },
+  {
+    title: "Which is the official language of Kerala?",
+    choices: ["Toda", "Malayalam","Divehi","Portuguese"],
+    answer: "Malayalam"
+  },
+  {
+    title: "How many districts does Himachal Pradesh consist? ",
+    choices: ["11", "13","12","10"],
+    answer: "12"
+  },
+  {
+    title: "Which is the official language of Himachal Pradesh? ",
+    choices: ["Santhali", "Hindi","Bodo","Nepali"],
+    answer: "Hindi"
+  },
+  {
+    title: "Which one is the biggest natural lake in Himachal Pradesh? ",
+    choices: ["Khajjiar Lake", "Brighu Lake","Renuka Lake","Rewalsar Lake"],
+    answer: "Renuka Lake"
+  },
+  {
+    title: "First chief minister of Himachal Pradesh (HP) ? ",
+    choices: ["Yashwant Singh Parmar", "Thakur Ram Lal","Jai Ram Thakur","Prem Kumar Dhumal"],
+    answer: "Yashwant Singh Parmar"
+  },
+  {
+    title: "When did Himachal Pradesh become a state?",
+    choices: ["25 January 1971", " 25 June 1964 ","07 July 1982","01 November 1956"],
+    answer: "25 January 1971"
+  },
+  {
+    title: "What is to the east of Andhra Pradesh?",
+    choices: ["Arabian Sea", "Bay of Bengal","Gulf of Cambay","Palk Strait"],
+    answer: "Bay of Bengal"
+  },
+  {
+    title: "When was Andhra Pradesh formed? ",
+    choices: ["04 March 1937", "08 June 1951","12 July 1949","01 October 1953"],
+    answer: "01 October 1953"
+  },
+  {
+    title: "Which state is to the south of Andhra Pradesh?",
+    choices: ["Orissa", "Madhya Pradesh","Tamil Nadu","Bihar"],
+    answer: "Tamil Nadu"
+  },
+  {
+    title: "Which of the Following is State fruit of Andhra Pradesh?",
+    choices: ["Orange", "Piece","Apple","Mango"],
+    answer: "Mango"
+  },
+  {
+    title: "Who is the first sportsperson to received Rajiv Gandhi Khel Ratna award from Andhra Pradesh?",
+    choices: ["Pullela Gopichand", "Karnam Malleswari","Saina Nehwal","Sania Mirza"],
+    answer: "Karnam Malleswari"
+  },
+  {
+    title: "Which city has the most population in Jammu and Kashmir?",
+    choices: ["Udhampur", "Jammu","Srinagar","Anantnag"],
+    answer: "Srinagar"
+  },
+  {
+    title: "Total number of national parks in Jammu and Kashmir?",
+    choices: ["4", "3","7","5"],
+    answer: "5"
+  },
+  {
+    title: "Pandrethan, the old city of Srinagar was founded by?",
+    choices: ["Gondana I", "Yudhistira","Ashoka","Gautam"],
+    answer: "Ashoka"
+  },
+  {
+    title: "Which town in Jammu and Kashmir is also known as Apple Town",
+    choices: ["Sopore", "Anantnag","Shupiyan","Jammu"],
+    answer: "Sopore"
+  },
+  {
+    title: "Between which mountain ranges is Kashmir Valley located?",
+    choices: ["Himadri - Pir Pranjal", "Shivalik- Himachal","Karakoram - Himachal","Himadri - Himachal "],
+    answer: "Himadri - Pir Pranjal"
+  },
+  {
+    title: "Telangana state formation day ? ",
+    choices: ["01,August", "02,June","24,July","19,September "],
+    answer: "02,June"
+  },
+  {
+    title: "Which of the following Village/City is famous for Paper industry?",
+    choices: ["Sirpur", "Bellampalle","Manchiryal","Sangareddy"],
+    answer: "Sirpur"
+  },
+  {
+    title: "Gnana Saraswati temple at Basara is located on the banks of the which river? ",
+    choices: ["Manjira river", "Krishna river","Godavari river"," Penganga river"],
+    answer: "Godavari river"
+  },
+  {
+    title: "Bathukamma is a part of which festival?",
+    choices: ["Ugadi", "Mahashivaratri","Dasara","Sankranti"],
+    answer: "Dasara"
+  },
+  {
+    title: "Bhongir fort situated in which district? ",
+    choices: ["Nizamabad", "Adilabad","Warangal","Nalgonda"],
+    answer: "Nalgonda"
+  },
+  {
+    title: "Which is the capital of Arunachal Pradesh?",
+    choices: ["Pasighat", "Itanagar","Deomali","Tezpur "],
+    answer: "Itanagar"
+  },
+  {
+    title: "When was Arunachal Pradesh separated from Assam?",
+    choices: ["1956", "1948","2000","1972"],
+    answer: "1972"
+  },
+  {
+    title: "Which is the Arunachal Pradesh state Bird?",
+    choices: ["Black necked crane", "Black crested bulbul","Great hornbill"," None of These"],
+    answer: "Great hornbill"
+  },
+  {
+    title: "When did Arunachal Pradesh state establish? ",
+    choices: ["20 Jan 1988", "20 Oct 1987","20 Feb 1987","15 Dec 1968 "],
+    answer: "20 Feb 1987"
+  },
+  {
+    title: "Namdapha National Park is located in  ",
+    choices: ["Changlang Arunachal Pradesh", "Bharatpur, Rajasthan","Nainital, Uttaranchal","None of These "],
+    answer: "Changlang Arunachal Pradesh"
+  },
+
+
+];
+
